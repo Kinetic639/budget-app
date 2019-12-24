364e7ba64bb371ed731935b75bb84d53cb7073ef
Michal Stepien <michal.stepien36@gmail.com>
2019-12-24 19:07:40 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.scss";
+import "normalize.css/normalize.css";
+import Header from "./core/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Działam</p>
+    </div>
+  );
+}
+
+export default App;
+ 
