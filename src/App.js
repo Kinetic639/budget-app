f41fe5069b440e0839105a161198da948bfc31e5
Michal Stepien <michal.stepien36@gmail.com>
2020-01-06 20:24:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,17 @@ import "./styles/styles.scss";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/ConfigureStore";
 import { addExpense } from "./actions/Expenses";
-import getVisibleExpenses from "./selectors/expenses";
-
-
-
-import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
-store.dispatch(addExpense({ description: "Water bill", amount: 4522, createdAt: 1578318461000, note: 'twoja stara nie ma kolan'   }));
+store.dispatch(
+  addExpense({
+    description: "Water bill",
+    amount: 4522,
+    createdAt: 1578318461000,
+    note: "twoja stara nie ma kolan"
+  })
+);
 store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
 store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
