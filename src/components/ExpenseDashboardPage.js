431aabca5b17af69f747fdbbd484c4fec3402329
Michal Stepien <michal.stepien36@gmail.com>
2020-01-14 21:09:59 +0100

    expenses summary

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ExpensesList from './ExpensesList'
-import ExpenseListFilters from './ExpenseListFilters'
-
-
+import React from "react";
+import ExpensesList from "./ExpensesList";
+import ExpenseListFilters from "./ExpenseListFilters";
+import ExpensesSummary from "./ExpensesSummary";
 
 export default function ExpenseDashboardPage(props) {
-    return (
-        <div>
-            <ExpenseListFilters />
-            <ExpensesList />
-        </div>
-    )
+  return (
+    <div>
+      <ExpenseListFilters />
+      <ExpensesSummary />
+      <ExpensesList />
+    </div>
+  );
 }
