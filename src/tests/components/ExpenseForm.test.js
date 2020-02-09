9e560a473f142e2c4dbe3a3132decd8dfb8d50f0
Michal Stepien <michal.stepien36@gmail.com>
2020-02-09 13:54:37 +0100

    enzyme finishind tests before deploy

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
+import moment from "moment";
 import ExpenseForm from "../../components/ExpenseForm";
 import expenses from "../fixtures/expenses";
 
-test("should render expense form", () => {
+test("should render ExpenseForm correctly", () => {
   const wrapper = shallow(<ExpenseForm />);
   expect(wrapper).toMatchSnapshot();
 });
 
-test("should render expense form with expense data", () => {
+test("should render ExpenseForm correctly with expense data", () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
@@ -16,13 +17,15 @@ test("should render expense form with expense data", () => {
 test("should render error for invalid form submission", () => {
   const wrapper = shallow(<ExpenseForm />);
   expect(wrapper).toMatchSnapshot();
-  wrapper.find("form").simulate("submit", { preventDefault: () => {} });
+  wrapper.find("form").simulate("submit", {
+    preventDefault: () => {}
+  });
   expect(wrapper.state("error").length).toBeGreaterThan(0);
   expect(wrapper).toMatchSnapshot();
 });
 
 test("should set description on input change", () => {
-  const value = "New Description";
+  const value = "New description";
   const wrapper = shallow(<ExpenseForm />);
   wrapper
     .find("input")
@@ -30,22 +33,19 @@ test("should set description on input change", () => {
     .simulate("change", {
       target: { value }
     });
-  expect(wrapper.state("description")).toEqual(value);
+  expect(wrapper.state("description")).toBe(value);
 });
 
 test("should set note on textarea change", () => {
-  const value = "New Note";
+  const value = "New note value";
   const wrapper = shallow(<ExpenseForm />);
-  wrapper
-    .find("textarea")
-    .at(0)
-    .simulate("change", {
-      target: { value }
-    });
-  expect(wrapper.state("note")).toEqual("New Note");
+  wrapper.find("textarea").simulate("change", {
+    target: { value }
+  });
+  expect(wrapper.state("note")).toBe(value);
 });
 
-test("should set amount if valid input - 23.50", () => {
+test("should set amount if valid input", () => {
   const value = "23.50";
   const wrapper = shallow(<ExpenseForm />);
   wrapper
@@ -54,11 +54,11 @@ test("should set amount if valid input - 23.50", () => {
     .simulate("change", {
       target: { value }
     });
-  expect(wrapper.state("amount")).toEqual(value);
+  expect(wrapper.state("amount")).toBe(value);
 });
 
 test("should not set amount if invalid input", () => {
-  const value = "21.122";
+  const value = "12.122";
   const wrapper = shallow(<ExpenseForm />);
   wrapper
     .find("input")
@@ -66,5 +66,36 @@ test("should not set amount if invalid input", () => {
     .simulate("change", {
       target: { value }
     });
-  expect(wrapper.state("amount")).toEqual("");
+  expect(wrapper.state("amount")).toBe("");
+});
+
+test("should call onSubmit prop for valid form submission", () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(
+    <ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />
+  );
+  wrapper.find("form").simulate("submit", {
+    preventDefault: () => {}
+  });
+  expect(wrapper.state("error")).toBe("");
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  });
+});
+
+test("should set new date on date change", () => {
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("#picker").prop("onDateChange")(now);
+  expect(wrapper.state("createdAt")).toEqual(now);
+});
+
+test("should set calendar focus on change", () => {
+  const focused = true;
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("#picker").prop("onFocusChange")({ focused });
+  expect(wrapper.state("calendarFocused")).toBe(focused);
 });
