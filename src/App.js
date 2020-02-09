7ceddb2c667e9494137de1877e70ef5b5889d26f
Michal Stepien <michal.stepien36@gmail.com>
2020-02-09 22:42:41 +0100

    dummy data removed

--- src/App.js
+++ src/App.js
@@ -12,20 +12,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({
-    description: "Water bill",
-    amount: 4507,
-    note: "Twoja stara nie ma kolan"
-  })
-);
-store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "rent", amount: 14507 }));
-
-console.log(store.getState());
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 const jsx = (
   <Provider store={store}>
     <AppRouter />
