1dce2c9e986ac8591f42dcce07a4f9da95756ee8
Michal Stepien <michal.stepien36@gmail.com>
2020-02-17 17:09:34 +0100

    add expenseSumamry to dashboard

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,28 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import numeral from "numeral";
-// load a locale
-numeral.register("locale", "pl", {
-  delimiters: {
-    thousands: " ",
-    decimal: ","
-  },
-  abbreviations: {
-    thousand: "k",
-    million: "m",
-    billion: "b",
-    trillion: "t"
-  },
-  ordinal: function(number) {
-    return number === 1 ? "er" : "ème";
-  },
-  currency: {
-    symbol: "zł"
-  }
-});
-
-// switch between locales
-numeral.locale("pl");
 
 const ExpenseListItem = ({ id, description, amount, createdAt, note }) => {
   return (
@@ -32,7 +10,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt, note }) => {
         <h3>{description}</h3>
       </Link>
       <p>
-        {numeral(amount / 100).format("0,0[.]00 $")}-
+        {numeral(amount / 100).format("$0,0.00")}-
         {moment(createdAt).format("DD, MM, YYYY")}
       </p>
     </li>
