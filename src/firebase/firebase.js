95c4bdb8e957ba35e37047e9509f51b2dac7588e
Michal Stepien <michal.stepien36@gmail.com>
2020-02-18 22:26:10 +0100

    thunk async actions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,57 +15,59 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().on("value", snapshot => {
-  const val = snapshot.val();
-  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-});
+export { firebase, database as default };
 
-// Setup data sub -> Andrew is a Software Developer at Amazon.
-
-// Change the data and make sure it reprints
-
-// database.ref('location/city')
-//   .once('value')
-//   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   })
-//   .catch((e) => {
-//     console.log('Error fetching data', e);
-//   });
-
-database
-  .ref()
-  .set({
-    name: "Andrew Mead",
-    age: 26,
-    stressLevel: 6,
-    job: {
-      title: "Software developer",
-      company: "Google"
-    },
-    location: {
-      city: "Philadelphia",
-      country: "United States"
-    }
-  })
-  .then(() => {
-    console.log("Data is saved!");
-  })
-  .catch(e => {
-    console.log("This failed.", e);
-  });
-
-// database.ref().update({
-//   stressLevel: 9,
-//   'job/company': 'Amazon',
-//   'location/city': 'Seattle'
+// database.ref().on("value", snapshot => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
 // });
 
-// database.ref()
-//   .remove()
+// // Setup data sub -> Andrew is a Software Developer at Amazon.
+
+// // Change the data and make sure it reprints
+
+// // database.ref('location/city')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(val);
+// //   })
+// //   .catch((e) => {
+// //     console.log('Error fetching data', e);
+// //   });
+
+// database
+//   .ref()
+//   .set({
+//     name: "Andrew Mead",
+//     age: 26,
+//     stressLevel: 6,
+//     job: {
+//       title: "Software developer",
+//       company: "Google"
+//     },
+//     location: {
+//       city: "Philadelphia",
+//       country: "United States"
+//     }
+//   })
 //   .then(() => {
-//     console.log('Data was removed');
-//   }).catch((e) => {
-//     console.log('Did not remove data', e);
+//     console.log("Data is saved!");
+//   })
+//   .catch(e => {
+//     console.log("This failed.", e);
 //   });
+
+// // database.ref().update({
+// //   stressLevel: 9,
+// //   'job/company': 'Amazon',
+// //   'location/city': 'Seattle'
+// // });
+
+// // database.ref()
+// //   .remove()
+// //   .then(() => {
+// //     console.log('Data was removed');
+// //   }).catch((e) => {
+// //     console.log('Did not remove data', e);
+// //   });
